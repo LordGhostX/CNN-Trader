add647ccce85f6e8083af098c6642a8b0c23e9d4
LordGhostX <solomonghost9@gmail.com>
2019-08-27 22:46:48 +0000

    rounded up generation

--- source/data_generator.py
+++ source/data_generator.py
@@ -1,8 +1,8 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from mpl_finance import candlestick_ohlc
-
 
 def generate_chart(chart_df, index):
     # reset df index to avoid errors from derived dataframe
@@ -24,13 +24,16 @@ def generate_chart(chart_df, index):
     ax1.axes.get_yaxis().set_visible(False)
 
     # save image
-    img_name = "data/images/chart-data-{}.png".format(index)
+    img_name = "data/images/chart-data-{}.jpg".format(index)
     plt.savefig(img_name)
 
     # convert to black and white then resize
-    img = Image.open(img_name).convert('LA')
-    img.resize((160, 120), Image.ANTIALIAS)
+    img = Image.open(img_name).convert('L')
+    img = img.resize((160, 120), Image.ANTIALIAS)
     img.save(img_name)
+
+    #print("Generated {}".format(img_name))
+    return np.array(img.getdata())
 
 if __name__ == "__main__":
     # params
@@ -38,10 +41,16 @@ if __name__ == "__main__":
     future_classification_level = 10
 
     # read csv data
-    df = pd.read_csv("data/BTCUSD_1h.csv")[:100]
+    df = pd.read_csv("data/BTCUSD_1h.csv")[::-1][:2000][::-1]
+    df.reset_index(inplace=True)
 
     # generate image data from csv
     index = 1
+    img_csv = ",".join(map(str, range(19200))) + ",target\n"
     for i in range(bars_per_image, len(df) - future_classification_level):
-        generate_chart(df[i - bars_per_image: i], index)
+        img_data = generate_chart(df[i - bars_per_image: i], index)
+        target = int(df.close[i] <= df.close[i + future_classification_level])
+        img_csv += ",".join(map(str, img_data)) + "," + str(target) + "\n"
         index += 1
+    with open("data/labels.csv", "w") as f:
+        f.write(img_csv)
